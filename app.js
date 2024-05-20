e4b882bee7e6d7ad58bf314df44157ac3ce415a7
mrinaljain <jain.mrinal140@gmail.com>
2024-05-20 16:53:38 +0530

    span tag

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 let root = document.getElementById("root");
 
-
+let span = React.createElement("span", {}, "span text");
+const element = React.createElement(
+   'h1',
+   null,
+   'Hello My name is ',
+   React.createElement('span', null, 'Mrinal'),
+   ' Jain'
+);
 let headingOne = React.createElement("h1", {}, "Hello world...!");
 let headingTwo = React.createElement("h2", {}, "Hello world once again");
 
-let div = React.createElement("div", { class: "halwa", id: "puri" }, [ headingOne, headingTwo,]);
+let div = React.createElement("div", { class: "halwa", id: "puri" }, [headingOne, headingTwo, element]);
 console.log(div);
 let dom = ReactDOM.createRoot(root);
 
