59ebff21c2622ef866194c2b45c09c37eb2c8d1f
mrinaljain <jain.mrinal140@gmail.com>
2024-06-17 20:45:51 +0530

    ui changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -22,7 +22,7 @@ function Home() {
     <div className="h-[100vh] p-5">
       <input
         type="text"
-        className="border-red-400 border-2 rounded flex justify-around w-80 py-2 px-2 mx-auto"
+        className="border-purple-300 border-4 placeholder:text-purple-500 rounded flex justify-around w-80 py-2 px-2 mx-auto"
         value={inputText}
         placeholder="Search"
         onChange={handleChange}
